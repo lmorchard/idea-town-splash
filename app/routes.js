cc0fc168a1f49ea1d671744d887547143d8963cf
John Gruen <john.gruen@gmail.com>
2015-07-28 15:47:06 -0400

    splashy

--- app/routes.js
+++ app/routes.js
@@ -2,47 +2,13 @@ module.exports = [
   {
     method: 'GET',
     path: '/',
-    handler: function(request,reply) {
-      reply.view('splash', require('./data/pages/splash.js'));
-    },
-    config: {
-      id: 'splash'
-    }
-  },
-
-  {
-    method: 'GET',
-    path: '/home',
-    handler: function(request,reply) {
-      reply.view('splash', require('./data/pages/home.js'));
+    handler: function(request, reply) {
+      reply.view('home', require('./data/pages/home.js'));
     },
     config: {
       id: 'home'
     }
   },
-
-  {
-    method: 'GET',
-    path: '/faq',
-    handler: function(request,reply) {
-      reply.view('splash', require('./data/pages/faq.js'));
-    },
-    config: {
-      id: 'faq'
-    }
-  },
-
-  {
-    method: 'GET',
-    path: '/privacy',
-    handler: function(request,reply) {
-      reply.view('splash', require('./data/pages/privacy.js'));
-    },
-    config: {
-      id: 'privacy'
-    }
-  },
-
   // Public
   {
     method: 'GET',
