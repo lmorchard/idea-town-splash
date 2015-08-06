0ac34db6c1c8b953a9a82602a6d4ba370766d79e
Dave Justice <davejustishh@gmail.com>
2015-08-06 15:42:12 -0700

    tweak styles and event handling for client side validation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,90 @@
 var isEmail = require('isemail');
 
+var signupForm = document.querySelector('#signup-form');
 var submitBtnEl  = document.querySelector('#signup');
 var errorMsgEl   = document.querySelector('.server-error');
 var invalidMsgEl = document.querySelector('.invalid-email');
 var successMsgEl = document.querySelector('.email-submitted');
-var emailEl = document.querySelector('#signupForm input#email');
+var emailEl = document.querySelector('#signup-form input#email');
 
-submitBtnEl.addEventListener('click', function(ev) {
-  ev.preventDefault();
+var SLOW = 1000;
+var FAST = 150;
+
+emailEl.addEventListener('keyup', function(ev) {
+
   var emailVal = emailEl.value;
+  var isValid = validateEmail(emailVal);
 
-  if (!validateEmail(emailVal)) {
-    emailEl.onkeyup = emailEl.onkeydown = emailEl.onkeypress = onInputChange;
-  } else {
-    emailEl.onkeyup = emailEl.onkeydown = emailEl.onkeypress = null;
-    makeRequest(emailVal);
+  if(ev.charCode !== 13) {
+    clearMessage(invalidMsgEl);
+    clearMessage(errorMsgEl);
   }
+
+  if(emailEl.classList.contains('error')) {
+    emailEl.classList.remove('error');
+  }
+
+  setSubmitButtonState(isValid);
 });
+
+signupForm.addEventListener('submit', function(ev) {
+  ev.preventDefault();
+
+  var emailVal = emailEl.value;
+  var isValid = validateEmail(emailVal);
+
+  if (isValid) {
+    makeRequest(emailVal);
+  } else {
+    showMessage(invalidMsgEl);
+    emailEl.focus();
+    emailEl.classList.add('error');
+  }
+
+});
+
+function validateEmail(value) {
+  return isEmail(value);
+}
+
+function setSubmitButtonState(isValid) {
+  if (isValid) {
+    submitBtnEl.classList.remove('disabled');
+  } else {
+    submitBtnEl.classList.add('disabled');
+  }
+}
+
+function showMessage(msgEl) {
+  if (msgEl.classList.contains('hidden')) {
+    msgEl.classList.remove('hidden');
+    addThenRemoveAnimationClass(msgEl, 'fade-in-pop-in', FAST, 'in');
+  } else {
+    addThenRemoveAnimationClass(msgEl, 'shake', SLOW, 'in');
+  }
+}
+
+function clearMessage(msgEl) {
+  if (!msgEl.classList.contains('hidden')) {
+    addThenRemoveAnimationClass(msgEl, 'fade-out', FAST, 'out');
+  }
+}
+
+function addThenRemoveAnimationClass(msgEl, className, timing, direction) {
+  msgEl.classList.add(className);
+  clearTimeout();
+  setTimeout(function() {
+    if (direction === 'out') {
+      msgEl.classList.add('hidden');
+    }
+    msgEl.classList.remove(className);
+  }, timing);
+}
 
 function makeRequest(emailValue) {
   var data = new FormData();
   data.append('email', emailValue);
+  submitBtnEl.disabled = true;
 
   var xhr = new XMLHttpRequest();
   xhr.open('POST', '/signup', true);
@@ -28,25 +92,12 @@ function makeRequest(emailValue) {
   xhr.send(data);
 }
 
-function onInputChange(ev) {
-  validateEmail(ev.target.value);
-}
-
-function validateEmail(value) {
-  if (isEmail(value)) {
-    invalidMsgEl.classList.add('hidden');
-    return true;
-  } else {
-    invalidMsgEl.classList.remove('hidden');
-    return false;
-  }
-}
-
 function onSubmitLoad(ev) {
   if (ev.target.status === 500) {
-    errorMsgEl.classList.remove('hidden');
+    showMessage(errorMsgEl);
+    submitBtnEl.disabled = false;
   } else {
-    successMsgEl.classList.remove('hidden');
-    submitBtnEl.disabled = true;
+    signupForm.classList.add('fade-out');
+    successMsgEl.classList.add('move-to-zero');
   }
 }
